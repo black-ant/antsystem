c37c90d6af34c7029012ce91c33bba560e29d8ea
black-ant <1016930479@qq.com>
2019-01-12 22:57:11 +0800

    负载均衡

--- client-platform/src/main/java/com/platform/platformclient/controller/DepartmentController.java
+++ client-platform/src/main/java/com/platform/platformclient/controller/DepartmentController.java
@@ -1,8 +1,19 @@
 package com.platform.platformclient.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class DepartmentController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/userall")
+    public String dc() {
+        return restTemplate.getForObject("http://datauser/platform/getUserAll", String.class);
+    }
 }
