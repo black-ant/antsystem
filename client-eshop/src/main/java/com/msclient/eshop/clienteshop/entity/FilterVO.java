9094f90f823ccf05d69538d8d719bac4120a7d6d
black-ant <1016930479@qq.com>
2019-02-12 22:59:34 +0800

    add

--- client-eshop/src/main/java/com/msclient/eshop/clienteshop/entity/FilterVO.java
+++ client-eshop/src/main/java/com/msclient/eshop/clienteshop/entity/FilterVO.java
@@ -2,6 +2,9 @@ package com.msclient.eshop.clienteshop.entity;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 /**
  * @author 10169
  * @Description 前端查询对象
@@ -11,8 +14,24 @@ import lombok.Data;
 @Data
 public class FilterVO {
     /**
-     *
      * 查询id
      */
     private String filterid;
+    /**
+     * 查询名称
+     */
+    private String filtername;
+    /**
+     * 查询关键字
+     */
+    private String filterkey;
+    /**
+     * 查询价格
+     */
+    private BigDecimal price;
+    /**
+     * 查询时间
+     */
+    private Date date;
+
 }
