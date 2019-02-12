e5bf8b8dcb2500531532764e71154073a2045275
black-ant <1016930479@qq.com>
2019-02-12 22:57:09 +0800

    add

---
+++ client-eshop/src/main/java/com/msclient/eshop/clienteshop/entity/FilterVO.java
@@ -0,0 +1,18 @@
+package com.msclient.eshop.clienteshop.entity;
+
+import lombok.Data;
+
+/**
+ * @author 10169
+ * @Description 前端查询对象
+ * @Date 2019/2/12 22:56
+ * @Version 1.0
+ **/
+@Data
+public class FilterVO {
+    /**
+     *
+     * 查询id
+     */
+    private String filterid;
+}
