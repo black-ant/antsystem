14f746d92688d27e9b5191ee3f59172be5001829
black-ant <1016930479@qq.com>
2019-03-24 23:24:39 +0800

    add

--- client-logistics/src/main/java/com/msclient/logistics/clientlogistics/ServerMapper/ServerOrderMapper.java
+++ client-logistics/src/main/java/com/msclient/logistics/clientlogistics/ServerMapper/ServerOrderMapper.java
@@ -1,5 +1,6 @@
 package com.msclient.logistics.clientlogistics.ServerMapper;
 
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
--- server-data-order/src/main/java/com/serverdata/order/serverdataorder/ServerDataOrderApplication.java
+++ server-data-order/src/main/java/com/serverdata/order/serverdataorder/ServerDataOrderApplication.java
@@ -2,8 +2,10 @@ package com.serverdata.order.serverdataorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServerDataOrderApplication {
 
     public static void main(String[] args) {
