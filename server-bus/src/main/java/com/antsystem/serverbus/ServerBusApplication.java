48cac44263782b00dfc14f23b3f70cb4c3b1f6f8
black-ant <1016930479@qq.com>
2019-04-05 23:37:16 +0800

    add

---
+++ server-bus/src/main/java/com/antsystem/serverbus/ServerBusApplication.java
@@ -0,0 +1,13 @@
+package com.antsystem.serverbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerBusApplication.class, args);
+    }
+
+}
---
+++ server-bus/src/test/java/com/antsystem/serverbus/ServerBusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.antsystem.serverbus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServerBusApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
